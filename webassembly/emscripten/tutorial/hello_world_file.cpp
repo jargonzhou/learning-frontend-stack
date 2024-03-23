a543f1bff359c2a3df5417f664dea92f0289a332
zhoujiagen <zhoujiagen@gmail.com>
2024-03-23 21:59:33 +0800

    [WebAssembly] Add example.

---
+++ webassembly/emscripten/tutorial/hello_world_file.cpp
@@ -0,0 +1,18 @@
+// Your C/C++ code can access files using the normal libc stdio API (fopen, fclose, etc.)
+#include <stdio.h>
+
+int main() {
+  FILE *file = fopen("hello_world_file.txt", "rb");
+  if (!file) {
+    printf("cannot open file\n");
+    return 1;
+  }
+  while (!feof(file)) {
+    char c = fgetc(file);
+    if (c != EOF) {
+      putchar(c);
+    }
+  }
+  fclose (file);
+  return 0;
+}
